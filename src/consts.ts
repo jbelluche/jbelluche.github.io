3621dd44f8667537db2e25c909300d75086534c5
Jack Belluche <jmbelluche@gmail.com>
2026-08-13 22:01:06 -0600

    Simplify blog layout

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jack Belluche';
-export const SITE_DESCRIPTION = 'Notes on software, ideas, and things worth thinking about.';
+export const SITE_DESCRIPTION = 'Personal blog.';
